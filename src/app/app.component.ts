d77bbf22cc89fe4537a386f71285411173693b56
AndresPaella <andresferre94@gmail.com>
2023-02-09 19:57:11 +0100

    Update code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Tweet} from "./share/model/tweet";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'twitterWeb3';
+  title = 'twitter-web3';
+
+  public tweets: Tweet[] =[];
+
+  public constructor() {
+    let tweet = new Tweet(new Date(), "Hello World", "John Doe", 0, [], [], []);
+    this.tweets.push(tweet);
+    tweet = new Tweet(new Date(), "This is the second tweet", "John Doe", 0, [], [], []);
+    this.tweets.push(tweet);
+    tweet = new Tweet(new Date(), "This is the third tweet", "John Doe", 0, [], [], []);
+    this.tweets.push(tweet);
+    tweet = new Tweet(new Date(), "This is the fourth tweet", "John Doe", 0, [], [], []);
+    this.tweets.push(tweet);
+
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TweetComponent } from './share/tweet/tweet.component';
+import {TweetComponent} from "./share/tweetcomponent/tweet.componet";
 
 @NgModule({
   declarations: [
     AppComponent,
-    TweetComponent
+    TweetComponent,
   ],
   imports: [
     BrowserModule,
